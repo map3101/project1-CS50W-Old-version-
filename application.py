bc8ddcb0ef6e57770850b464ce137cd9f9acce79
map3101 <marcelo.postiglione@gmail.com>
2020-04-22 06:38:47 -0300

    Register function first version

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
 import os
+from forms import *
+from models import *
 
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'thisissecret'
+
+bootstrap = Bootstrap(app)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,7 +27,32 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '<h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserForm()
+
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, password=form.password.data)
+        
+        if db.execute("SELECT username FROM users WHERE username = :new_user", {"new_user": new_user.username}).rowcount == 1:
+            return "<h1>USERNAME NOT AVAILABLE</h1>"
+
+        else:
+            db.execute("INSERT INTO users (username, password) VALUES (:new_username, :new_password)", {"new_username": new_user.username, "new_password": new_user.password})
+            db.commit()
+            return 'USER CREATED'
+
+    return render_template('signup.html', form=form)
---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+#User form to register or login
+class UserForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
